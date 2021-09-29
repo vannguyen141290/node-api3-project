97c81e070318260180a9becc8b07c271ddac0cee
Sanoo Singh <vannguyen141290@gmail.com>
2021-09-29 15:02:48 -0700

    testing submission

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// require your server and launch it
+const server = require('./api/server');
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+});
+
